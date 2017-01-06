58b9a9c6489806b379324341b29e682f2136382b
821193332 <bieweiwoku@163.com>
2017-01-06 19:01:55 +0800

    第一阶段

--- app/src/main/java/com/kkkk/l/mobile/WelcomeActivity.java
+++ app/src/main/java/com/kkkk/l/mobile/WelcomeActivity.java
@@ -1,8 +1,10 @@
 package com.kkkk.l.mobile;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
 
 public class WelcomeActivity extends Activity {
     private Handler handler =new Handler();
@@ -14,9 +16,25 @@ public class WelcomeActivity extends Activity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-
+                statrtMainActivity();
             }
         },2000);
     }
+    private boolean isStartMain =false;
+  private  void statrtMainActivity(){
+       startActivity(new Intent(this,MainActivity.class));
+      finish();
+  }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        statrtMainActivity();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
 }
---
+++ app/src/main/java/com/kkkk/l/mobile/view/TitleBarView.java
@@ -0,0 +1,15 @@
+package com.kkkk.l.mobile.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by KangYueLong on 2017/1/6.
+ */
+
+public class TitleBarView extends LinearLayout{
+    public TitleBarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
